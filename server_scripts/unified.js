845b871d2cba68cf7cafa50df42a73e7c5e772d6
Bruno Marques <bruno@bmarques.net>
2025-06-30 18:35:17 +0100

    Fixed ore loot unification

--- server_scripts/unified.js
+++ server_scripts/unified.js
@@ -1,6 +1,6 @@
 // make ores always return the preferred material variant
 LootJS.modifiers(e => {
-    e.addBlockModifier('#forge:ores').modifyLoot('#forge:raw_materials', item => {
+    e.addBlockModifier('#c:ores').modifyLoot('#c:raw_materials', item => {
         const target = AlmostUnified.getVariantItemTarget(item);
         if (target.isEmpty()) {
             return item;
