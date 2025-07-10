6047ab12ba2ea8ba201d053e68904166b108ea3f
Bruno Marques <bruno@bmarques.net>
2025-07-10 13:02:04 +0100

    whoops

--- push-to-modpack.js
+++ push-to-modpack.js
@@ -6,5 +6,5 @@ import 'dotenv/config';
 for (const src of ['config/', 'kubejs/']) {
     const dst = path.join(process.env.MODPACK_ROOT, src);
     await fs.copy(src, dst);
-    console.log('done - remember to /reload!');
 }
+console.log('done - remember to /reload!');
